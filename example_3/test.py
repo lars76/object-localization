c13c57affb82748c72d4e909110f269164408b86
Lars Nieradzik <l.nieradzik@gmail.com>
2018-10-30 02:02:20 +0100

    example_3: support multiple objects

--- example_3/test.py
+++ example_3/test.py
@@ -4,6 +4,8 @@ import glob
 
 WEIGHTS_FILE = "model-0.34.h5"
 IMAGES = "../images/*jpg"
+THRESHOLD = 0.5
+EPSILON = 0.02
 
 def main():
     model = create_model()
@@ -16,29 +18,28 @@ def main():
 
         region = model.predict(x=np.array([feat_scaled]))[0]
 
-        boxes = []
-        for j in range(region.shape[1]):
-            for i in range(region.shape[0]):
-                if region[i][j] > 0.5:
+        output = np.zeros(unscaled.shape[:2], dtype=np.uint8)
+        for i in range(region.shape[1]):
+            for j in range(region.shape[0]):
+                if region[i][j] > THRESHOLD:
                     x = int(CELL_WIDTH * j * unscaled.shape[1] / IMAGE_WIDTH)
                     y = int(CELL_HEIGHT * i * unscaled.shape[0] / IMAGE_HEIGHT)
                     x2 = int(CELL_WIDTH * (j + 1) * unscaled.shape[1] / IMAGE_WIDTH)
                     y2 = int(CELL_HEIGHT * (i + 1) * unscaled.shape[0] / IMAGE_HEIGHT)
-                    #cv2.rectangle(unscaled, (x, y), (x2, y2), (0, 255, 0), 2)
-                    if not boxes or boxes[-1][2] < x:
-                        boxes.append([x, y, x2, y2])
-                    else:
-                        boxes[-1][0] = min(x, boxes[-1][0])
-                        boxes[-1][1] = min(y, boxes[-1][1])
-                        boxes[-1][2] = max(x2, boxes[-1][2])
-                        boxes[-1][3] = max(y2, boxes[-1][3])
+                    #cv2.rectangle(unscaled, (x, y), (x2, y2), (0, 255, 0), 1)
 
-        for box in boxes:
-            cv2.rectangle(unscaled, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 1)
+                    output[y:y2,x:x2] = 1
+
+        _, contours, _ = cv2.findContours(output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in contours:
+            approx = cv2.approxPolyDP(cnt, EPSILON * cv2.arcLength(cnt, True), True)
+            x, y, w, h = cv2.boundingRect(approx)
+            cv2.rectangle(unscaled, (x, y), (x + w, y + h), (0, 255, 0), 1);
 
         cv2.imshow("image", unscaled)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
