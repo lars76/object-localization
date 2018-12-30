83ffc98621602dcd6b0b29f7994762294f28d883
Lars Nieradzik <l.nieradzik@gmail.com>
2018-12-30 06:56:29 +0100

    example_4: use sigmoid, fixed val loss

--- example_4/train.py
+++ example_4/train.py
@@ -22,7 +22,7 @@ IMAGE_SIZE = 224
 
 # first train with frozen weights, then fine tune
 TRAINABLE = False
-WEIGHTS = "model-0.50.h5"
+WEIGHTS = "model-0.15.h5"
 
 EPOCHS = 200
 BATCH_SIZE = 32
@@ -30,7 +30,7 @@ PATIENCE = 15
 LEARNING_RATE = 1e-4
 
 MULTITHREADING = True
-THREADS = 8
+THREADS = 4
 
 TRAIN_CSV = "train.csv"
 VALIDATION_CSV = "validation.csv"
@@ -148,16 +148,13 @@ class DataGenerator(Sequence):
             mid_x = 1 / image_width * (x0 + (x1 - x0) / 2)
             mid_y = 1 / image_height * (y0 + (y1 - y0) / 2)
 
-            w = (x1 - x0) / image_width
-            h = (y1 - y0) / image_height
-
             unrounded_x = min(mid_x * GRID_SIZE, GRID_SIZE)
             unrounded_y = min(mid_y * GRID_SIZE, GRID_SIZE)
 
-            cell_x = int(np.floor(unrounded_x))
-            cell_y = int(np.floor(unrounded_y))
-            cell_w = (x1 - x0) * GRID_SIZE / image_width
-            cell_h = (y1 - y0) * GRID_SIZE / image_height
+            cell_x = math.floor(unrounded_x)
+            cell_y = math.floor(unrounded_y)
+            cell_w = (x1 - x0) / image_width
+            cell_h = (y1 - y0) / image_height
 
             batch_boxes[i, :, :, 0] = cell_h
             batch_boxes[i, :, :, 1] = cell_w
@@ -172,11 +169,11 @@ class DataGenerator(Sequence):
 
                 changed = Image.fromarray(changed)
 
-                h = cell_h * IMAGE_SIZE / GRID_SIZE
-                w = cell_w * IMAGE_SIZE / GRID_SIZE
+                h = cell_h * IMAGE_SIZE
+                w = cell_w * IMAGE_SIZE
 
-                y0 = unrounded_y * IMAGE_SIZE / GRID_SIZE - h / 2
-                x0 = unrounded_x * IMAGE_SIZE / GRID_SIZE - w / 2
+                y0 = unrounded_y * IMAGE_SIZE - h / 2
+                x0 = unrounded_x * IMAGE_SIZE - w / 2
                 y1 = y0 + h
                 x1 = x0 + w
 
@@ -189,20 +186,18 @@ class DataGenerator(Sequence):
 
 
 class Validation(Callback):
-    def get_box(self, mask):
-        reshaped = mask.reshape(mask.shape[0], mask.shape[1] * mask.shape[2], -1)
+    def get_box_highest_percentage(self, mask):
+        reshaped = mask.reshape(mask.shape[0], np.prod(mask.shape[1:-1]), -1)
 
         score_ind = np.argmax(reshaped[...,-1], axis=-1)
+        unraveled = np.array(np.unravel_index(score_ind, mask.shape[:-1])).T[:,1:]
 
-        height = reshaped[range(reshaped.shape[0]), score_ind, 0]
-        width = reshaped[range(reshaped.shape[0]), score_ind, 1]
-        offset_y = reshaped[range(reshaped.shape[0]), score_ind, 2]
-        offset_x = reshaped[range(reshaped.shape[0]), score_ind, 3]
+        cell_y, cell_x = unraveled[...,0], unraveled[...,1]
+        boxes = mask[np.arange(mask.shape[0]), cell_y, cell_x]
 
-        y = score_ind // mask.shape[2]
-        x = score_ind % mask.shape[2]
+        offset_y, offset_x, h, w = boxes[...,0], boxes[...,1], boxes[...,2], boxes[...,3]
 
-        return np.stack([x + offset_x + 1, y + offset_y + 1, width, height], axis=-1)
+        return np.stack([cell_y + offset_y + 1, cell_x + offset_x + 1, h, w], axis=-1)
 
     def __init__(self, generator):
         self.generator = generator
@@ -216,16 +211,16 @@ class Validation(Callback):
             batch_images, gt = self.generator[i]
             pred = self.model.predict_on_batch(batch_images)
 
-            pred = self.get_box(pred)
-            gt = self.get_box(gt)
+            pred = self.get_box_highest_percentage(pred)
+            gt = self.get_box_highest_percentage(gt)
 
             mse += np.linalg.norm(gt - pred, ord='fro') / pred.shape[0]
 
             pred = np.maximum(pred, 0)
             gt = np.maximum(gt, 0)
 
-            diff_width = np.minimum(gt[:,0] + gt[:,2], pred[:,0] + pred[:,2]) - np.maximum(gt[:,0], pred[:,0])
-            diff_height = np.minimum(gt[:,1] + gt[:,3], pred[:,1] + pred[:,3]) - np.maximum(gt[:,1], pred[:,1])
+            diff_height = np.minimum(gt[:,0] + gt[:,2], pred[:,0] + pred[:,2]) - np.maximum(gt[:,0], pred[:,0])
+            diff_width = np.minimum(gt[:,1] + gt[:,3], pred[:,1] + pred[:,3]) - np.maximum(gt[:,1], pred[:,1])
             intersection = np.maximum(diff_width, 0) * np.maximum(diff_height, 0)
 
             area_gt = gt[:,2] * gt[:,3]
@@ -243,19 +238,6 @@ class Validation(Callback):
 
         print(" - val_iou: {} - val_mse: {}".format(iou, mse))
 
-def clip(boxes):
-    _, height, width, _ = boxes.shape
-    height = tf.cast(height, tf.float32)
-    width = tf.cast(width, tf.float32)
-
-    h = tf.clip_by_value(boxes[..., 0], epsilon(), height)
-    w = tf.clip_by_value(boxes[..., 1], epsilon(), width)
-    y = tf.clip_by_value(boxes[..., 2], epsilon(), 1.0)
-    x = tf.clip_by_value(boxes[..., 3], epsilon(), 1.0)
-    s = tf.clip_by_value(boxes[..., 4], epsilon(), 1.0)
-
-    return tf.stack([h, w, y, x, s], axis=-1)
-
 def create_model(trainable=False):
     model = MobileNetV2(input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3), include_top=False, alpha=ALPHA, weights="imagenet")
 
@@ -268,57 +250,75 @@ def create_model(trainable=False):
     x = BatchNormalization()(x)
     x = Activation("relu")(x)
 
-    x = Conv2D(5, padding="same", kernel_size=1, activation=lambda l : tf.concat([l[...,:4], tf.sigmoid(l[...,4:5])], axis=-1))(x)
-    x = Lambda(clip)(x)
+    x = Conv2D(5, padding="same", kernel_size=1, activation="sigmoid")(x)
 
     return Model(inputs=model.input, outputs=x)
 
 def detection_loss(iou_threshold=0.5):
     def get_box_highest_percentage(arr):
         shape = tf.shape(arr)
-        batch, height, width = shape[0], shape[1], shape[2]
 
-        reshaped = tf.reshape(arr, (batch, height * width, -1))
+        reshaped = tf.reshape(arr, (shape[0], tf.reduce_prod(shape[1:-1]), -1))
 
-        max_prob_ind = tf.argmax(reshaped[...,-1], axis=1, output_type=tf.int32)
-        indices = tf.stack([tf.range(batch), max_prob_ind], axis=-1)
+        # returns array containing the index of the highest percentage of each batch
+        # where 0 <= index <= height * width * anchors
+        max_prob_ind = tf.argmax(reshaped[...,-1], axis=-1, output_type=tf.int32)
 
-        height, width = tf.cast(height, tf.float32), tf.cast(width, tf.float32)
-        y, x = tf.cast(indices[...,1:2], tf.float32) // height, tf.cast(indices[...,1:2], tf.float32) % width
+        # turn indices (batch, y * x) into (batch, y, x)
+        # returns (3, batch) tensor
+        unraveled = tf.unravel_index(max_prob_ind, shape[:-1])
 
-        out = tf.concat([y, x, tf.gather_nd(reshaped, indices)], axis=-1)
-        out = tf.concat([out[...,:1] + out[...,4:5] + 1, out[...,1:2] + out[...,5:6] + 1, out[...,2:4], out[...,6:7]], axis=-1)
+        # turn tensor into (batch, 3) and keep only (y, x)
+        unraveled = tf.transpose(unraveled)[:,1:]
+        y, x = unraveled[...,0], unraveled[...,1]
+
+        # stack indices and create (batch, 5) tensor which
+        # contains height, width, offset_y, offset_x, percentage
+        indices = tf.stack([tf.range(shape[0]), y, x], axis=-1)
+        box = tf.gather_nd(arr, indices)
+
+        y, x = tf.cast(y, tf.float32), tf.cast(x, tf.float32)
+
+        # transform box to (y + offset_y + 1, x + offset_x + 1, height, width, obj)
+        # output is (batch, 5)
+        out = tf.stack([y + box[...,2] + 1, x + box[...,3] + 1,
+                         box[...,0], box[...,1], box[...,-1]], axis=-1)
 
         return out
 
-    def calculate_iou(true_box, pred_size):
-        shape = tf.shape(pred_size)
+    def calculate_iou(true_box, pred_boxes):
+        shape = tf.shape(pred_boxes)
         batch, height, width = shape[0], shape[1], shape[2]
 
-        flattened = tf.reshape(tf.range(height * width), (1, -1))
-        flattened = tf.tile(flattened, [batch, 1])
-        flattened = tf.reshape(flattened, (-1, height * width, 1))
+        # create x and y coordinates for pred_boxes (cell_x, cell_y)
+        ys, xs = tf.meshgrid(tf.range(height), tf.range(width))
 
-        ys = tf.cast(flattened, tf.float32) // tf.cast(height, tf.float32)
-        xs = tf.cast(flattened, tf.float32) % tf.cast(width, tf.float32)
-        merged = tf.stack([ys, xs], axis=-1)
-        merged = tf.reshape(merged, (batch, height, width, -1))
+        cell_y = tf.reshape(ys, (1, width, height))
+        cell_y = tf.cast(tf.tile(cell_y, [batch, 1, 1]), tf.float32)
 
-        pred_boxes = tf.concat([merged[...,:1] + pred_size[...,2:3] + 1, merged[...,1:2] + pred_size[...,3:4] + 1, pred_size[...,:2]], axis=-1)
-        pred_boxes = tf.reshape(pred_boxes, (batch, height, width, -1))
+        cell_x = tf.reshape(xs, (1, width, height))
+        cell_x = tf.cast(tf.tile(cell_x, [batch, 1, 1]), tf.float32)
 
-        true_boxes = tf.reshape(true_box, (batch, 1, -1))
-        true_boxes = tf.tile(true_boxes, [1, height * width, 1])
-        true_boxes = tf.reshape(true_boxes, (batch, height, width, -1))
+        # transform box
+        # output is (batch, height, width, 5)
+        pred = tf.stack([cell_y + pred_boxes[...,2] + 1, cell_x + pred_boxes[...,3] + 1,
+                         pred_boxes[...,0], pred_boxes[...,1], pred_boxes[...,-1]], axis=-1)
 
-        y0 = tf.maximum(true_boxes[...,0], pred_boxes[...,0])
-        x0 = tf.maximum(true_boxes[...,1], pred_boxes[...,1])
-        y1 = tf.minimum(true_boxes[...,0] + true_boxes[...,2], pred_boxes[...,0] + pred_boxes[...,2])
-        x1 = tf.minimum(true_boxes[...,1] + true_boxes[...,3], pred_boxes[...,1] + pred_boxes[...,3])
+        # reshape (batch, 5) to (batch, height, width, 5)
+        gt = tf.reshape(true_box, (batch, 1, 1, -1))
+        gt = tf.tile(gt, [1, height, width, 1])
+
+        # calculate iou
+        y0 = tf.maximum(gt[...,0], pred[...,0])
+        x0 = tf.maximum(gt[...,1], pred[...,1])
+        y1 = tf.minimum(gt[...,0] + gt[...,2], pred[...,0] + pred[...,2])
+        x1 = tf.minimum(gt[...,1] + gt[...,3], pred[...,1] + pred[...,3])
 
         intersection = (x1 - x0) * (y1 - y0)
-        union = true_boxes[...,2] * true_boxes[...,3] + pred_boxes[...,2] * pred_boxes[...,3]
+        union = gt[...,2] * gt[...,3] + pred[...,2] * pred[...,3]
         res = tf.clip_by_value(intersection / (union - intersection + epsilon()), epsilon(), 1)
+
+        # reshape (batch, height, width) to (batch, height, width, 1)
         res = tf.reshape(res, (batch, height, width, 1))
 
         return res
@@ -330,14 +330,16 @@ def detection_loss(iou_threshold=0.5):
         true_box = get_box_highest_percentage(y_true)
         pred_box = get_box_highest_percentage(y_pred)
 
-        iou = calculate_iou(true_box, y_pred[...,:-1])
+        iou = calculate_iou(true_box, y_pred)
         mask = tf.cast(tf.greater(iou, iou_threshold) | tf.equal(obj_true, 1), tf.float32)
 
         obj_loss = binary_crossentropy(mask, obj_pred)
         size_loss = tf.reduce_mean(tf.squared_difference(true_box[...,:2], pred_box[...,:2]))
         coord_loss = tf.reduce_mean(tf.squared_difference(true_box[...,2:], pred_box[...,2:]))
 
-        return obj_loss + size_loss + coord_loss
+        all_obj = tf.reduce_mean(iou * tf.squared_difference(y_true[...,:-1], y_pred[...,:-1]))
+
+        return obj_loss + size_loss + coord_loss + all_obj
 
     return loss
 
